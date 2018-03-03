877ecf73dd1342d496e47267cf508064cd409e00
Stephanie Angulo <angulostephanie21@gmail.com>
2018-03-03 15:13:07 -0800

    can navigate to search xml page

--- app/src/main/java/com/example/stephanieangulo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/stephanieangulo/myapplication/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.stephanieangulo.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Context mContext;
+    private Button searchBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
+        searchBtn = findViewById(R.id.searchPageBtn);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detailIntent = new Intent(mContext, SearchActivity.class);
+                // put title and instruction URL
+
+                startActivity(detailIntent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/stephanieangulo/myapplication/SearchActivity.java
+++ app/src/main/java/com/example/stephanieangulo/myapplication/SearchActivity.java
@@ -1,8 +1,16 @@
 package com.example.stephanieangulo.myapplication;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by stephanieangulo on 3/1/18.
  */
 
-public class SearchActivity {
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
 }
